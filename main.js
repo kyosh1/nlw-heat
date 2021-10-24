448ec7de458d9b2d639d38f017f31731aa2aafeb
kyosh1 <92681851+kyosh1@users.noreply.github.com>
2021-10-24 20:08:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const LinksSocialMedia = {
+    github: 'kyosh1',
+    youtube: 'channel/UCHC9iVPnGfMuvWvCSBKf9Lw',
+    facebook: 'kyosh1',
+    instagram: 'kyosh1',
+    twitter: 'kyosh1'
+}
+
+function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class');
+
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+        console.log(li.children[0].href)
+    }
+}
+
+async function getGithubProfile() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
+
+    const res = await axios.get(url);
+    const { name, bio, avatar_url } = res.data;
+
+    username.textContent = name;
+    avatar.src = avatar_url;
+    bioU.textContent = bio || "As a developer focused on web programming";
+}
+
+getGithubProfile()
+changeSocialMediaLinks()
